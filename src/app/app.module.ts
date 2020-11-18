fb4dd723886fa5529cdf039f8c6adeda3fe03a9f
Mmb <medmehdibelghali@gmail.com>
2020-11-18 18:46:59 +0100

    First push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,30 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {
+  GoogleApiModule, 
+  GoogleApiService, 
+  GoogleAuthService, 
+  NgGapiClientConfig, 
+  NG_GAPI_CONFIG,
+  GoogleApiConfig
+} from "ng-gapi";
+
+let gapiClientConfig: NgGapiClientConfig = {
+  client_id: "469802607999-eeqabie4pldo5f8o81194n0np6ppkmmj.apps.googleusercontent.com",
+  discoveryDocs: ["https://analyticsreporting.googleapis.com/v4/userActivity:search"],
+  scope: [
+    "email"
+  ].join(" ")
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,GoogleApiModule.forRoot({
+    provide: NG_GAPI_CONFIG,
+    useValue: gapiClientConfig
+  }),],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import '@codetrix-studio/capacitor-google-auth';
+import { Plugins } from '@capacitor/core';
+import { GoogleApiService, GoogleAuthService } from 'ng-gapi';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public static SESSION_STORAGE_KEY: string = 'accessToken';
+  userInfo = null;
+  user : any;
+  googleUser : any;
+
+  constructor(private gapiService : GoogleApiService,private googleAuth : GoogleAuthService) {
+  }
+
+  private signInSuccessHandler(res: any) {
+    this.user = res;
+    sessionStorage.setItem(
+        HomePage.SESSION_STORAGE_KEY, res.getAuthResponse().access_token
+    );
+    this.userInfo = res.tt.$t;
+}
+
+  async googleSingIn() {
+  /*  this.googleAuth.getAuth()
+            .subscribe((auth) => {
+                auth.signIn().then(res => this.signInSuccessHandler(res));
+            });*/
+   try { 
+    this.googleUser = await Plugins.GoogleAuth.signIn(null);
+    console.log('user :', this.googleUser);
+   this.userInfo = this.googleUser.email; }
+   catch (err) {
+     console.log(err);
+     this.userInfo = Plugins.GoogleAuth.signIn(null)
+   } 
+  }
+  
+  
 
 }
